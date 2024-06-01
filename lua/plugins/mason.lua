2c29293c96faf8edc7ea1ba89795ff495989c7fd
Gerome Meyer <gerome.meyer@protonmail.com>
2024-06-01 07:56:19 +0200

    Added Terraform LSP and TS plugins

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -21,6 +21,7 @@ return {
         -- "gopls",
         "julials",
         "taplo",
+        "terraformls",
       })
     end,
   },
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -17,6 +17,7 @@ return {
       "markdown",
       "nix",
       "julia",
+      "terraform",
     })
   end,
 }
