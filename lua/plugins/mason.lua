7ede34df6d749f8bcd393a3082615e477a6e201e
Gerome Meyer <gerome.meyer@eawag.ch>
2024-10-22 10:15:28 +0200

    Python: Replaced black with ruff

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -34,7 +34,7 @@ return {
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
         "prettier",
         "stylua",
-        "black",
+        "ruff",
         "isort",
       })
     end,
